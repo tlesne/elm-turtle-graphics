a4a112d9e43650c509db23103706b56c5193ef5b
Max Goldstein <maxgoldstein1@gmail.com>
2015-05-17 16:04:14 -0400

    Handle pen state when teleporting, moving

--- Turtle.elm
+++ Turtle.elm
@@ -6,6 +6,7 @@ import Graphics.Element exposing (show, Element)
 import Graphics.Collage as C exposing (defaultLine)
 import Color exposing (Color)
 import Window
+import Debug
 
 import List.Nonempty as NE exposing (Nonempty, (:::))
 
@@ -32,14 +33,22 @@ ngon n =
     let step = Make [Forward 25, Right (360 / toFloat n)]
     in List.repeat n step
 
+invisibly : Step -> Step
+invisibly x = Make
+    [ PenUp
+    , x
+    , PenDown
+    ]
+
 turtle : Movement
 turtle =
     [ Make <| ngon 8
     , Pen Color.blue
-    , PenUp
-    , Teleport (-100, 0)
-    , PenDown
+    , invisibly <| Teleport (-100, 0)
     , Make <| ngon 7
+    , invisibly <| Teleport (-300, 0)
+    , Pen Color.red
+    , Make <| ngon 9
     ]
 
 type alias Coord = (Float, Float)
@@ -61,23 +70,26 @@ changeFigure state f =
     then {state|figures <- NE.replaceHead (f <| NE.head state.figures) state.figures}
     else changeFigure (newFigure state) f
 
-{-| Run the tutle and immediately show the result in a collage of the size (think `Window.dimensions`). Useful for
-rapidly iterating code, and for use with `Signal.map` and dynamic controls.
+{-| Run the turtle and immediately show the result in a collage of the given size (think `Window.dimensions`). Useful
+for rapidly iterating code, and for use with `Signal.map` and dynamic controls.
 -}
 drawTurtle : Movement -> (Int, Int) -> Element
 drawTurtle m dims =
     let state0 : ParseState
         state0 = ParseState (degrees 90) 1 True <| NE.fromElement (Figure Color.black (NE.fromElement (0,0)))
+        moveTo : ParseState -> Coord -> ParseState
+        moveTo state pos =
+            if not state.penDown
+            then changeFigure state (\fig -> {fig| path <- NE.fromElement pos})
+            else let oldFigure = NE.head state.figures
+                     newFigure = {oldFigure| path <- pos ::: oldFigure.path}
+                 in {state| figures <- NE.replaceHead newFigure state.figures}
         parse1 : Step -> ParseState -> ParseState
         parse1 step state = case step of
-            Forward d -> if not state.penDown then state else
-                         let (x0, y0) = NE.head (NE.head state.figures).path
+            Forward d -> let (x0, y0) = NE.head (NE.head state.figures).path
                              dx = state.scale * d * cos state.theta
                              dy = state.scale * d * sin state.theta
-                             pos = (x0+dx, y0+dy)
-                             oldFigure = NE.head state.figures
-                             newFigure = {oldFigure| path <- pos ::: oldFigure.path}
-                          in {state| figures <- NE.replaceHead newFigure state.figures}
+                         in moveTo state (x0+dx, y0+dy)
             Back d -> parse1 (Forward -d) state
             Right ang -> {state| theta <- state.theta - (degrees ang)}
             Left ang -> {state| theta <- state.theta + (degrees ang)}
@@ -86,7 +98,8 @@ drawTurtle m dims =
             PenUp -> {state| penDown <- False}
             PenDown -> {state| penDown <- True}
             Randomized f -> parse1 (f 0.5) state
-            Teleport newPos -> changeFigure state (\fig -> {fig|path <- NE.fromElement newPos})
+            Teleport newPos -> moveTo state newPos
+            --let fig = NE.head state.figures in {state| figures <- NE.replaceHead {fig|path <- newPos ::: fig.path} state.figures}
             RotateTo newTheta -> {state| theta <- newTheta}
             Make ms -> parse state ms
             _ -> state
@@ -97,6 +110,8 @@ drawTurtle m dims =
             List.map (\fig -> ({defaultLine| color <- fig.color}, NE.toList fig.path)) (NE.toList state.figures)
         drawing : ParseState
         drawing = parse state0 m
+        _ = Debug.log "number of figures" (NE.length drawing.figures)
+        _ = Debug.log "drawing" drawing
     in render dims drawing
 
 main = Signal.map (drawTurtle turtle) Window.dimensions
