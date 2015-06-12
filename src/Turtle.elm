77e425fd93229193d69060da0ef82acaa3ffb570
Max Goldstein <maxgoldstein1@gmail.com>
2015-06-11 23:37:50 -0400

    Add a few more functions to the basics

--- src/Turtle.elm
+++ src/Turtle.elm
@@ -4,6 +4,7 @@ module Turtle where
 
 import Turtle.Core as Core
 import Graphics.Element exposing (Element)
+import Color exposing (Color)
 
 type alias Step = Core.Step
 
@@ -19,8 +20,22 @@ make = Core.Make
 scale : Float -> Step
 scale = Core.Scale
 
+penUp : Step
+penUp = Core.PenUp
+
+penDown : Step
+penDown = Core.PenDown
+
+penColor : Color -> Step
+penColor = Core.Pen
+
+{-| Animate the turtle drawing by showing the progressive steps.
+-}
 animate : List Step -> Signal Element
 animate = Core.animate
 
+{-| Run the turtle and immediately show the result in a collage of the given size (think `Window.dimensions`). Useful
+for rapidly prototyping code, and for use with `Signal.map` and dynamic controls.
+-}
 draw : List Step -> (Int,Int) -> Element
 draw = Core.draw
