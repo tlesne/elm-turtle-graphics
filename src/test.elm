41f6feeba71590748d270cb9c40e360140d8ac14
Max Goldstein <maxgoldstein1@gmail.com>
2015-06-16 21:29:55 -0400

    Testing makes a star but needs refinement

--- src/test.elm
+++ src/test.elm
@@ -1,7 +1,10 @@
-import Turtle
+import Turtle exposing (Step)
 import Turtle.Advanced as Turtle
 
 r = 100
 
-main = Turtle.animate
-    [Turtle.forward r, Turtle.back r, Turtle.circle r]
+main = Turtle.animate (star 5)
+
+star n =
+    let dt = 360 / toFloat n
+    in (Turtle.right 90 :: List.repeat n (Turtle.make [Turtle.forward r, Turtle.right (2*dt), Turtle.forward r, Turtle.left dt]))
